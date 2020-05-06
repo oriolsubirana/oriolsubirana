30529682f12ef555fb075fc216011c76da9e7678
oriolsubirana <subi.cat@gmail.com>
2020-05-06 14:11:15 +0200

    about me

--- src/styles/about/AboutStyles.js
+++ src/styles/about/AboutStyles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const AboutPageWrapper = styled.div`
   margin-top: 100px;
   min-height: 70vh;
-
+  text-align: justify;
+  text-justify: inter-word;
   h1 {
     font-size: 2.7rem;
     color: ${props => props.theme.textColor};
